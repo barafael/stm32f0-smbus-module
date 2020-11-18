93a82ccee4bbd6ed179eeabdb1febfbdea782e18
Rafael Bachmann <mediumendian@gmail.com>
2020-11-18 11:39:06 +0100

    Add initial i2c config

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-#![deny(unsafe_code)]
-#![deny(warnings)]
+//#![deny(unsafe_code)]
+//#![deny(warnings)]
 #![no_std]
 #![no_main]
 
@@ -7,12 +7,19 @@ use panic_halt as _;
 use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
 
+use crate::pac::I2C1;
+use stm32f0xx_hal::{gpio::gpiob::PB8, rcc};
+use stm32f0xx_hal::gpio::gpiob::PB9;
+use stm32f0xx_hal::gpio::Alternate;
+use stm32f0xx_hal::gpio::AF1;
+
 use stm32f0xx_hal::{
     gpio::gpioa::PA5,
     gpio::gpioc::PC13,
-    gpio::{Input, Output, PullUp, PushPull},
+    gpio::{Input, Output, PushPull, Floating},
     pac,
     prelude::*,
+    i2c::I2c,
 };
 
 use cortex_m::interrupt::free as disable_interrupts;
@@ -21,8 +28,9 @@ use cortex_m::interrupt::free as disable_interrupts;
 const APP: () = {
     struct Resources {
         exti: pac::EXTI,
-        user_button: PC13<Input<PullUp>>,
+        user_button: PC13<Input<Floating>>,
         led: PA5<Output<PushPull>>,
+        i2c: I2C1,
     }
 
     #[init]
@@ -48,7 +56,7 @@ const APP: () = {
         let led = disable_interrupts(|cs| gpioa.pa5.into_push_pull_output(cs));
 
         let gpioc = dp.GPIOC.split(&mut rcc);
-        let user_button = disable_interrupts(|cs| gpioc.pc13.into_pull_up_input(cs));
+        let user_button = disable_interrupts(|cs| gpioc.pc13.into_floating_input(cs));
 
         dp.SYSCFG.exticr4.write(|w| w.exti13().pc13());
 
@@ -56,26 +64,58 @@ const APP: () = {
         dp.EXTI.imr.write(|w| w.mr13().set_bit());
 
         // Set interrupt rising trigger
+        dp.EXTI.ftsr.write(|w| w.tr13().set_bit());
         dp.EXTI.rtsr.write(|w| w.tr13().set_bit());
 
         rprintln!("Instantiating dp.EXTI...");
         let exti = dp.EXTI;
         rprintln!("Defining late resources...");
 
+        let gpiob = dp.GPIOB.split(&mut rcc);
+
+        let (scl, sda): (PB8<Alternate<AF1>>, PB9<Alternate<AF1>>) = disable_interrupts(|cs| {
+            (
+            gpiob.pb8.into_alternate_af1(cs),
+            gpiob.pb9.into_alternate_af1(cs)
+            )
+        });
+
+        unsafe {
+            dp.I2C1.icr.write(|w| w.bits(0xFF));
+
+            dp.I2C1.cr2.modify(|_, w| w.autoend().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.gcen().set_bit());
+            dp.I2C1.oar2.modify(|_, w| w.oa2en().clear_bit());
+            dp.I2C1.cr1.modify(|_, w| w.nostretch().clear_bit());
+            dp.I2C1.cr1.modify(|_, w| w.pe().clear_bit());
+            dp.I2C1.cr1.modify(|_, w| w.anfoff().set_bit().dnf().no_filter());
+            dp.I2C1.cr1.modify(|_, w| w.pe().set_bit());
+            dp.I2C1.oar1.modify(|_, w| w.oa1en().set_bit());
+            dp.I2C1.oar1.write(|w| w.oa1().bits(0x4e));
+            dp.I2C1.oar1.modify(|_, w| w.oa1en().set_bit());
+            //dp.I2C1.cr1.modify(|_, w| w.smbhen().set_bit().smbden().set_bit())
+        }
+
+        //I2c<I2C1, PB8<Alternate<AF1>>, PB9<Alternate<AF1>>>,
         init::LateResources {
             exti,
             user_button,
             led,
+            i2c: dp.I2C1
         }
     }
 
-    #[idle(resources = [led])]
+    #[idle(resources = [])]
     fn idle(_: idle::Context) -> ! {
         loop {
             cortex_m::asm::nop();
+            // disable wfe for now (no probe-rs)
             //cortex_m::asm::wfe();
         }
     }
+
+    #[task(binds = I2C1, resources = [exti, user_button, led])]
+    fn i2c1_interrupt(ctx: i2c1_interrupt::Context) { }
 
     #[task(binds = EXTI4_15, resources = [exti, user_button, led])]
     fn exti_4_15_interrupt(ctx: exti_4_15_interrupt::Context) {
