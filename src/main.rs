217bed4f7f05e23148195e4e0618adc7d78a6b0b
Rafael Bachmann <mediumendian@gmail.com>
2020-11-26 17:35:05 +0100

    More I2C slave config

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,43 @@ use panic_halt as _;
 use rtic::app;
 use rtt_target::{rprintln, rtt_init_print};
 
-use crate::pac::I2C1;
+use stm32f0xx_hal::gpio::gpiob::PB8;
 use stm32f0xx_hal::gpio::gpiob::PB9;
 use stm32f0xx_hal::gpio::Alternate;
 use stm32f0xx_hal::gpio::AF1;
-use stm32f0xx_hal::{gpio::gpiob::PB8, rcc};
 
 use stm32f0xx_hal::{
     gpio::gpioa::PA5,
     gpio::gpioc::PC13,
     gpio::{Floating, Input, Output, PushPull},
-    i2c::I2c,
     pac,
     prelude::*,
-    stm32f0,
 };
 
 use cortex_m::interrupt::free as disable_interrupts;
+
+#[derive(Debug)]
+enum SMBusDirection {
+    SlaveToMaster,
+    MasterToSlave,
+}
+
+impl Default for SMBusDirection {
+    fn default() -> Self {
+        SMBusDirection::MasterToSlave
+    }
+}
+
+const transmission_size: usize = 32;
+
+#[derive(Default, Debug)]
+pub struct SMBusState {
+    dir: SMBusDirection,
+    dataReady: bool,
+    bytesToTransmit: usize,
+    receive_buffer: [u8; transmission_size],
+    transmit_buffer: [u8; transmission_size],
+}
 
 #[app(device = stm32f0xx_hal::pac, peripherals = true)]
 const APP: () = {
@@ -31,7 +51,8 @@ const APP: () = {
         exti: pac::EXTI,
         user_button: PC13<Input<Floating>>,
         led: PA5<Output<PushPull>>,
-        i2c: I2C1,
+        i2c: pac::I2C1,
+        transmissionState: SMBusState,
     }
 
     #[init]
@@ -42,11 +63,17 @@ const APP: () = {
         // Alias peripherals
         let mut dp: pac::Peripherals = ctx.device;
 
+        rprintln!("Initializing clocks");
+
         dp.RCC.apb2enr.modify(|_, w| w.syscfgen().set_bit());
-        dp.RCC.apb1enr.modify(|_, w| w.i2c1en().set_bit());
+        dp.RCC
+            .apb1enr
+            .modify(|_, w| w.i2c1en().set_bit().pwren().set_bit());
         dp.RCC.cfgr3.modify(|_, w| w.i2c1sw().sysclk());
+        dp.RCC.ahbenr.modify(|_, w| w.iopben().set_bit());
 
         rprintln!("Initializing peripherals");
+
         let mut rcc = dp
             .RCC
             .configure()
@@ -88,24 +115,40 @@ const APP: () = {
         const I2C_MODE_SMBUS_AUTOEND_WITH_PEC: u32 = (1 << 25) | (1 << 26);
 
         unsafe {
-            dp.I2C1.icr.write(|w| w.bits(0xFF));
+            dp.I2C1.icr.reset();
 
             dp.I2C1.cr2.modify(|_, w| w.autoend().set_bit());
-            dp.I2C1.cr1.modify(|_, w| w.gcen().set_bit());
+            dp.I2C1.oar1.modify(|_, w| w.oa1en().clear_bit());
             dp.I2C1.oar2.modify(|_, w| w.oa2en().clear_bit());
+            dp.I2C1.cr1.modify(|_, w| w.gcen().set_bit());
             dp.I2C1.cr1.modify(|_, w| w.nostretch().clear_bit());
             dp.I2C1.cr1.modify(|_, w| w.pe().clear_bit());
             dp.I2C1
                 .cr1
                 .modify(|_, w| w.anfoff().set_bit().dnf().no_filter());
             dp.I2C1.cr1.modify(|_, w| w.pe().set_bit());
-            dp.I2C1.oar1.modify(|_, w| w.oa1en().set_bit());
-            dp.I2C1.oar1.write(|w| w.oa1().bits(0x4e));
-            dp.I2C1.oar1.modify(|_, w| w.oa1en().set_bit());
+            dp.I2C1.oar1.write(|w| w.oa1().bits(0x33));
             dp.I2C1
                 .cr1
                 .modify(|r, w| w.bits(r.bits() | I2C_MODE_SMBUS_AUTOEND_WITH_PEC));
+            //dp.I2C1.cr2.modify(|_, w| w.nack().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.pecen().set_bit());
+            dp.I2C1.oar1.modify(|_, w| w.oa1en().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.sbc().set_bit());
+
+            dp.I2C1.cr1.modify(|_, w| w.txie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.rxie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.addrie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.nackie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.stopie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.tcie().set_bit());
+            dp.I2C1.cr1.modify(|_, w| w.errie().set_bit());
+            dp.I2C1.cr2.modify(|_, w| w.nbytes().bits(0x1));
         }
+
+        //let a: bool = dp.I2C1.isr.read().dir();
+
+        let state = SMBusState::default();
 
         //I2c<I2C1, PB8<Alternate<AF1>>, PB9<Alternate<AF1>>>,
         init::LateResources {
@@ -113,6 +156,7 @@ const APP: () = {
             user_button,
             led,
             i2c: dp.I2C1,
+            transmissionState: state,
         }
     }
 
@@ -125,8 +169,35 @@ const APP: () = {
         }
     }
 
-    #[task(binds = I2C1, resources = [exti, user_button, led])]
-    fn i2c1_interrupt(ctx: i2c1_interrupt::Context) {}
+    #[task(binds = I2C1, resources = [i2c, user_button, led, transmissionState], priority = 1)]
+    fn i2c1_interrupt(ctx: i2c1_interrupt::Context) {
+        rprintln!("Something happened on SMBUS!");
+        let isrReg = ctx.resources.i2c.isr.read().bits();
+        let dataReg = ctx.resources.i2c.rxdr.read().bits();
+
+        if ctx.resources.i2c.isr.read().addr().is_match_() {
+            if ctx.resources.i2c.isr.read().dir().is_read() {
+                ctx.resources.transmissionState.dir = SMBusDirection::SlaveToMaster;
+                //trcount = 0;
+                execute_smbus_command(ctx.resources.transmissionState);
+                //ctx.resources.i2c.isr.txe().set_bit();
+            }
+        }
+        /*
+        if (isrReg & 1 << 3) == 1 << 3 {
+            if (isrReg & 1 << 16) == 1 << 16 {
+
+            }
+        }
+        */
+
+        rprintln!("{}", ctx.resources.i2c.isr.read().bits());
+        ctx.resources.i2c.icr.write(|w| w.addrcf().set_bit());
+        ctx.resources.i2c.icr.reset();
+        unsafe {
+            ctx.resources.i2c.icr.write(|w| w.bits(0xFF));
+        }
+    }
 
     #[task(binds = EXTI4_15, resources = [exti, user_button, led])]
     fn exti_4_15_interrupt(ctx: exti_4_15_interrupt::Context) {
